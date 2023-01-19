dbd5d81f7c81f8ea25f8e92110b73fcdcbc094f9
yung-coder <chandel.sanjeev5@gmail.com>
2023-01-19 22:21:53 +0530

    package fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import './index.css'
 function App() {
 
   return (
-    <div className="text-3xl text-purple-500">
+    <div className="text-3xl text-red-600">
       Demo live testing
     </div>
   )
