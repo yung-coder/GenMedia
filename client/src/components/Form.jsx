430a2991e9baa6eb5d2f0fdddd97bd681f6f3532
yung-coder <chandel.sanjeev5@gmail.com>
2023-01-21 21:46:40 +0530

    raw form

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -72,9 +72,7 @@ const Form = () => {
             <div class="container mx-auto">
               <div class="flex justify-center px-6 my-12">
                 <div class="w-full xl:w-3/4 lg:w-11/12 flex">
-                  <div
-                    class="w-full h-auto bg-gray-400 hidden lg:block lg:w-1/2 bg-cover rounded-l-lg"
-                  ></div>
+                  <div class="w-full h-auto bg-gray-400 hidden lg:block lg:w-1/2 bg-cover rounded-l-lg"></div>
 
                   <div class="w-full lg:w-1/2 bg-white p-5 rounded-lg lg:rounded-l-none">
                     <h3 class="pt-4 text-2xl text-center">Welcome Back!</h3>
@@ -101,24 +99,11 @@ const Form = () => {
                           Password
                         </label>
                         <input
-                          class="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border border-red-500 rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                          class="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border  rounded shadow appearance-none focus:outline-none focus:shadow-outline"
                           id="password"
                           type="password"
                           placeholder="******************"
                         />
-                        <p class="text-xs italic text-red-500">
-                          Please choose a password.
-                        </p>
-                      </div>
-                      <div class="mb-4">
-                        <input
-                          class="mr-2 leading-tight"
-                          type="checkbox"
-                          id="checkbox_id"
-                        />
-                        <label class="text-sm" for="checkbox_id">
-                          Remember Me
-                        </label>
                       </div>
                       <div class="mb-6 text-center">
                         <button
@@ -132,7 +117,10 @@ const Form = () => {
                       <div class="text-center">
                         <a
                           class="inline-block text-sm text-blue-500 align-baseline hover:text-blue-800"
-                          href="./register.html"
+                          onClick={() => {
+                            setPageType(isLogin ? "register" : "login");
+                            resetForm();
+                          }}
                         >
                           Create an Account!
                         </a>
@@ -143,6 +131,136 @@ const Form = () => {
                           href="./forgot-password.html"
                         >
                           Forgot Password?
+                        </a>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
+
+          {isRegister && (
+            <div class="container mx-auto">
+              <div class="flex justify-center px-6 my-12">
+                <div class="w-full xl:w-3/4 lg:w-11/12 flex">
+                  <div class="w-full h-auto bg-gray-400 hidden lg:block lg:w-5/12 bg-cover rounded-l-lg"></div>
+
+                  <div class="w-full lg:w-7/12 bg-white p-5 rounded-lg lg:rounded-l-none">
+                    <h3 class="pt-4 text-2xl text-center">
+                      Create an Account!
+                    </h3>
+                    <form class="px-8 pt-6 pb-8 mb-4 bg-white rounded">
+                      <div class="mb-4 md:flex md:justify-between">
+                        <div class="mb-4 md:mr-2 md:mb-0">
+                          <label
+                            class="block mb-2 text-sm font-bold text-gray-700"
+                            for="firstName"
+                          >
+                            First Name
+                          </label>
+                          <input
+                            class="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                            id="firstName"
+                            type="text"
+                            placeholder="First Name"
+                            error={
+                              Boolean(touched.firstName) &&
+                              Boolean(errors.firstName)
+                            }
+                            helperText={touched.firstName && errors.firstName}
+                          />
+                        </div>
+                        <div class="md:ml-2">
+                          <label
+                            class="block mb-2 text-sm font-bold text-gray-700"
+                            for="lastName"
+                          >
+                            Last Name
+                          </label>
+                          <input
+                            class="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                            id="lastName"
+                            type="text"
+                            placeholder="Last Name"
+                            error={Boolean(touched.lastName) && Boolean(errors.lastName)}
+                            helperText={touched.lastName && errors.lastName}
+                          />
+                        </div>
+                      </div>
+                      <div class="mb-4">
+                        <label
+                          class="block mb-2 text-sm font-bold text-gray-700"
+                          for="email"
+                        >
+                          Email
+                        </label>
+                        <input
+                          class="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                          id="email"
+                          type="email"
+                          placeholder="Email"
+                        />
+                      </div>
+                      <div class="mb-4 md:flex md:justify-between">
+                        <div class="mb-4 md:mr-2 md:mb-0">
+                          <label
+                            class="block mb-2 text-sm font-bold text-gray-700"
+                            for="password"
+                          >
+                            Password
+                          </label>
+                          <input
+                            class="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border border-red-500 rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                            id="password"
+                            type="password"
+                            placeholder="******************"
+                          />
+                          <p class="text-xs italic text-red-500">
+                            Please choose a password.
+                          </p>
+                        </div>
+                        <div class="md:ml-2">
+                          <label
+                            class="block mb-2 text-sm font-bold text-gray-700"
+                            for="c_password"
+                          >
+                            Confirm Password
+                          </label>
+                          <input
+                            class="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                            id="c_password"
+                            type="password"
+                            placeholder="******************"
+                          />
+                        </div>
+                      </div>
+                      <div class="mb-6 text-center">
+                        <button
+                          class="w-full px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700 focus:outline-none focus:shadow-outline"
+                          type="button"
+                        >
+                          Register Account
+                        </button>
+                      </div>
+                      <hr class="mb-6 border-t" />
+                      <div class="text-center">
+                        <a
+                          class="inline-block text-sm text-blue-500 align-baseline hover:text-blue-800"
+                          href="#"
+                        >
+                          Forgot Password?
+                        </a>
+                      </div>
+                      <div class="text-center">
+                        <a
+                          class="inline-block text-sm text-blue-500 align-baseline hover:text-blue-800"
+                          onClick={() => {
+                            setPageType(isLogin ? "register" : "login");
+                            resetForm();
+                          }}
+                        >
+                          Already have an account? Login!
                         </a>
                       </div>
                     </form>
