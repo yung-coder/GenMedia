18dae34d86b8debb26d5df0f72bffacb1a9eedfe
yung-coder <chandel.sanjeev5@gmail.com>
2023-01-25 01:10:17 +0530

    raw

--- client/src/components/UserImage.jsx
+++ client/src/components/UserImage.jsx
@@ -2,8 +2,12 @@ import React from "react";
 
 const UserImage = ({ image }) => {
   return (
-    <div className="rounded-xl">
-      <img src={`http://localhost:3001/assets/${image}`} alt="userimage" />
+    <div className="rounded-full w-fit h-fit">
+      <img
+        src={`http://localhost:3001/assets/${image}`}
+        alt="userimage"
+        className="w-16 h-16 rounded-full"
+      />
     </div>
   );
 };
--- client/src/scenes/widgets/UserWidget.jsx
+++ client/src/scenes/widgets/UserWidget.jsx
@@ -3,6 +3,9 @@ import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { setLogin } from "../../state/index";
 import UserImage from "../../components/UserImage";
+import design from "../../../public/design.jpg";
+import { GrLocation } from "react-icons/gr";
+import { MdWork } from "react-icons/md";
 
 const UserWidget = ({ userId, picturePath }) => {
   console.log(typeof picturePath);
@@ -39,16 +42,33 @@ const UserWidget = ({ userId, picturePath }) => {
   } = user;
 
   return (
-    <div>
-      <div className="flex flex-col">
-        <div>
-          <UserImage image={picturePath}/>
+    <div className="border relative">
+      <img src={design} alt="" className="w-full h-16" />
+      <div className="flex flex-col absolute top-1/4 left-1/4 border">
+        <div className="flex justify-center items-center">
+          <UserImage image={picturePath} />
         </div>
-        <div>
-            {firstName}
+        <div className="flex justify-center items-center">{firstName}</div>
+        <div className="flex flex-col">
+          <div className="flex space-x-4">
+            <h1 className="text-gray-300">Impressions</h1>
+            <h1>{impressions}</h1>
+          </div>
+          <div className="flex space-x-4">
+            <h1 className="text-gray-300">Profle Views</h1>
+            <h1 className="">{viewwdProfile}</h1>
+          </div>
         </div>
-        <div></div>
-        <div></div>
+        <div className="flex space-x-4 justify-center items-center">
+          <div className="flex flex-col space-y-3">
+            <GrLocation />
+            <h1>{loacation}</h1>
+          </div>
+          <div className="flex flex-col space-y-3">
+            <MdWork />
+            <h1>{occupation}</h1>
+          </div>
+        </div>
       </div>
     </div>
   );
