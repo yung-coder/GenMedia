a841da5ce74f2271305cba2827dbdaf765918385
yung-coder <chandel.sanjeev5@gmail.com>
2023-01-30 01:32:02 +0530

    fixing

--- client/src/scenes/Welcome.jsx
+++ client/src/scenes/Welcome.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom';
 const Welcome = () => {
     const navigate = useNavigate();
   return (
-    <div>
+    <div className='w-screen h-screen'>
         <button onClick={() => navigate('/login')}>Login</button>
         <button onClick={() => navigate('/register')}>Register</button>
     </div>
